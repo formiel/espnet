73bf4fcf66a94fb158b06e540d9b8f73e3f22607
D. Khuê Lê-Huu <huudienkhue.le@gmail.com>
2020-03-28 22:56:11 +0100

    Added __init__s

--- egs/an4/asr1/path.sh
+++ egs/an4/asr1/path.sh
@@ -7,12 +7,12 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build
-if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
-    source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
-else
-    source $MAIN_ROOT/tools/venv/bin/activate
-fi
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build
+# if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
+#     source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
+# else
+#     source $MAIN_ROOT/tools/venv/bin/activate
+# fi
 export PATH=$MAIN_ROOT/utils:$MAIN_ROOT/espnet/bin:$PATH
 
 export OMP_NUM_THREADS=1
